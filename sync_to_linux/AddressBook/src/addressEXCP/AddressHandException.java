0a21497ae3b109adfd5fba5435a86a7d2c48d91a
dev2user <poproot4@gmail.com>
2023-05-07 08:53:08 +0530

    automated update

--- sync_to_linux/AddressBook/src/addressEXCP/AddressHandException.java
+++
@@ -1,7 +0,0 @@
-package addressEXCP;
-public class AddressHandException extends Exception{
-	private static final long serialVersionUID = 1L;
-	public AddressHandException(String msg) {
-		super(msg);
-	}
-}
--- sync_to_linux/productlist1/src/productexception/ProductException.java
+++
@@ -1,4 +0,0 @@
-package productexception;
-public class ProductException extends Exception {
-private static final long serialVersionUID = 1L;
-public ProductException(String message){super(message);}}
